f3671b5c670c23936ea06232ca030afa48be65c9
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-12-01 13:40:40 +0100

    Ajout de la possibilité de cliquer sur le bouton et de faire apparaitre les vidéos

--- js/video.js
+++ js/video.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container")
+const btnVideo = document.querySelector(".btn-video")
+console.log(btnVideo)
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 130) {
@@ -9,4 +11,9 @@ window.addEventListener("scroll", () => {
 })
 window.addEventListener("load", () => {
   container.style.display = "none"
+})
+
+btnVideo.addEventListener("click", () => {
+  console.log("hello")
+  container.style.display = "flex"
 })
