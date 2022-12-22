1e75bd60d36fd5741cf57d65b3c30bde19cfaef5
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-12-22 19:24:54 +0100

    Ajout d'une margin a l'input recherche et suppression des console log dans le fichier js

--- js/armes.js
+++ js/armes.js
@@ -1,17 +1,12 @@
 function filter() {
   const filtre = document.getElementById("maRecherche").value.toUpperCase()
-  console.log(filtre, "filtre")
   const tableau = document.getElementById("tableau")
-  console.log(tableau, "tableau")
   const ligne = document.getElementsByClassName("weapon-container")
-  console.log(ligne, "ligne")
 
   for (i = 0; i < ligne.length; i++) {
     const cellule = ligne[i].querySelectorAll(".title-arme")[0]
-    console.log(cellule, "cellule")
     if (cellule) {
       const texte = cellule.innerText
-      console.log(texte, "texte")
       if (texte.toUpperCase().indexOf(filtre) > -1) {
         ligne[i].style.display = ""
       } else {
